771af6a34bd412a9f90b1ceb915f906ae291a6dd
Belal-Wael <belalbatnain050@gmail.com>
2025-11-15 19:36:58 +0200

    Added multiplyNumbers feature

--- UtilityTool.java
+++ UtilityTool.java
@@ -4,12 +4,17 @@ public class UtilityTool {
         System.out.println("Welcome to My Utility Tool!");
         System.out.println("1 + 2 = " + addNumbers(1, 2));
         System.out.println("Current Time: " + getCurrentTime());
+        System.out.println("3 * 4 = " + multiplyNumbers(3, 4));
+
     }
 
     public static int addNumbers(int a, int b) {
         return a + b;
     }
-    
+    public static int multiplyNumbers(int a, int b) {
+    return a * b;
+}
+
     public static String getCurrentTime() {
         return java.time.LocalTime.now().toString();
     }
